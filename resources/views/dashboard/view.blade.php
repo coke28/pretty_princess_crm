272f12c825a2a9942f4e2566408a4059453fca71
KohkieIzumi <maveric.izumi@rvnsolutions.com>
2023-09-21 13:57:34 +0800

    Added dashboard cards and styling

--- resources/views/dashboard/view.blade.php
+++ resources/views/dashboard/view.blade.php
@@ -1,13 +1,76 @@
 <x-CRM-layout :pageTitle="$pageTitle" :pageDescription="$pageDescription">
+  <style>.grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr); /* Creates a 2x2 grid */
+    gap: 20px; /* Adjust the gap between cards as needed */
+  }
 
+  .card {
+    background-color: white; /* Set card background color to white */
+    border: none; /* Remove the card border */
+    padding: 0; /* Remove padding to match grid size */
+    width: 100%; /* Set card width to 100% to match grid size */
+    display: flex; /* Use flex to stretch card content */
+    flex-direction: column; /* Stack card content vertically */
+  }
+
+  .card-content {
+    text-align: center;
+    background-color: white; /* Set card content background color to white */
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    flex: 1; /* Allow card content to stretch vertically */
+  }
+
+  .card-title {
+    font-size: 18px;
+    font-weight: bold;
+    color: #16c1c0; /* Set card title text color to blue */
+  }
+
+  .card-text {
+    font-size: 16px;
+    color: #16c1c0; /* Set card text color to blue */
+  }
+
+  .online-user-count {
+    font-size: 20px;
+    font-weight: bold;
+    color: #16c1c0; /* Set online user count text color to blue */
+  }
+  </style>
   <!--start::Include your modals here-->
-  <div class="card">
-    <div class="card-body">
+  <div class="grid">
+    <div class="card">
+      <div class="card-content">
         <h5 class="card-title">Online Users</h5>
         <p class="card-text">Number of users currently online:</p>
-        <p>Online Users: <span id="online-user-count">Loading...</span></p>
+        <p>Online Users: <span class="online-user-count" id="online-user-count">Loading...</span></p>
+      </div>
     </div>
-</div>
+  
+    <div class="card">
+      <div class="card-content">
+        <h5 class="card-title">Number Of Active Forms</h5>
+        <p class="card-text">Number of forms currently active:</p>
+        <p>Online Users: <span class="online-user-count" id="online-user-count">Loading...</span></p>
+      </div>
+    </div>
+  
+    <div class="card">
+      <div class="card-content">
+        <!-- Add content for the third card -->
+      </div>
+    </div>
+  
+    <div class="card">
+      <div class="card-content">
+        <!-- Add content for the fourth card -->
+      </div>
+    </div>
+  </div>
+
 
   <!--start::Include your scripts here-->
   @section('scripts')
